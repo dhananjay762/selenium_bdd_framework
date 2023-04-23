deefe8f7d8c0a7f2e44bbf61d087eb0127326601
Dhananjay <dhananjay762@gmail.com>
2023-04-23 16:31:16 +0530

    updated file

--- src/test/java/com/automation/test/Runner.java
+++ src/test/java/com/automation/test/Runner.java
@@ -1,8 +1,11 @@
 package com.automation.test;
 
 import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
+
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import cucumber.api.testng.TestNGCucumberRunner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 
@@ -10,9 +13,9 @@ import cucumber.api.SnippetType;
 @CucumberOptions(
 		features = {
 				"src/test/resources/com/automation/features/Test_OrderMensJacket.feature",
-				"src/test/resources/com/automation/features/New_Test.feature"
+//				"src/test/resources/com/automation/features/New_Test.feature"
 				},
-		glue= {"com/automation/stepdefs", "com/automation/support", "com/automation/listener"},
+		glue= {"com/automation/stepdefs", "com/automation/support", "com/automation/listener", "com/automation/base"},
 		plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/test-report/TestReport.html"},
 	//	plugin = {"pretty", "json:target/json/report.json", "html:target/cucumber"},
 		monochrome = true,
@@ -24,6 +27,15 @@ import cucumber.api.SnippetType;
 
 public class Runner extends AbstractTestNGCucumberTests{
 	
-	
+//	TestNGCucumberRunner testNGCucumberRunner;
+//	
+//	@DataProvider(name="myFeatures")
+//	public Object[][] getFeatures()
+//	{
+//	    if(testNGCucumberRunner == null){
+//	        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+//	    }
+//	    return testNGCucumberRunner.provideFeatures();
+//	}
 		
 }
